8dfb6f16ee2e5d22756592710ed648865b607a3b
Jarek Kajko <jaruto7@gmail.com>
2019-08-11 21:36:23 +0200

    Style form-editor in main.scss

--- src/js/main.js
+++ src/js/main.js
@@ -1,23 +1,29 @@
-"use strict";
-
-// Pobierz wszystkie przyciski i zapisz w zmiennych
+// Pobierz wszystkie przyciski i inne elementy, i zapisz je w zmiennych
 const textarea = document.querySelector('.header__textarea--js');
 const buttonDelete = document.querySelector('.button--delete-js');
 const buttonLoad = document.querySelector('.button--load-js');
 const buttonSave = document.querySelector('.button--save-js');
 
+// Nasłuchuj zdarzenie na kliknięcie przycisku Save przez użytkownika
 buttonSave.addEventListener('click', (e) => {
+  // Nie pozwól odświeżyć się stronie po kliknięciu 
   e.preventDefault();
-  // console.log(JSON.stringify(textarea.value));
+  // Zapisz dane do local storage które zostaną przechowane nawet jeśli strona zostanie zamknięta
   localStorage.setItem("textarea", textarea.value);
 });
 
+// Nasłuchuj zdarzenie na kliknięcie przycisku Load przez użytkownika
 buttonLoad.addEventListener('click', (e) => {
+  // Nie pozwól odświeżyć się stronie po kliknięciu 
   e.preventDefault();
+  // Pobierz dane z local storage i wyświetl w obszarze textarea
   textarea.innerHTML = localStorage.getItem("textarea", e.target.value);
 });
 
+// Nasłuchuj zdarzenie na kliknięcie przycisku Delete przez użytkownika
 buttonDelete.addEventListener('click', (e) => {
+  // Nie pozwól odświeżyć się stronie po kliknięciu 
   e.preventDefault();
+  //Usuń trwałe dane z local storage
   localStorage.removeItem("textarea");
 });
